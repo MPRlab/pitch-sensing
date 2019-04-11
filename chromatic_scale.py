44a4834c08575d8f1c61c0ad3a19d694325901f6
Craig Miller <craigdanielmiller@gmail.com>
2019-04-11 18:41:12 -0400

    Made plots optional
    Made plots optional.  Fixed frequency range definition.  Renamed function.

--- chromatic_scale.py
+++ chromatic_scale.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from note_frequencies import freqs
 
-def plot_chromatic_scale(ioi,start_freq_idx,end_freq_idx,dt=0.01):
+def chromatic_scale(ioi,start_freq_idx,end_freq_idx,dt=0.01, plot=False):
     '''Calculates and plots ideal frequency values over time 
     for chromatic scale.'''
     start_time=0
@@ -15,32 +15,31 @@ def plot_chromatic_scale(ioi,start_freq_idx,end_freq_idx,dt=0.01):
     y=[]
     
     #Ascending
-    for freq_idx in range(start_freq_idx,end_freq_idx):
+    for freq_idx in range(start_freq_idx,end_freq_idx+1):
         for idx, t in enumerate(np.arange(start_time,stop_time,dt)):
             y.append(freqs[freq_idx])
             time.append(t)
-        start_time=stop_time
+        start_time=stop_time+dt
         stop_time=start_time+ioi
 
     #Descending
-    for freq_idx in reversed(range(start_freq_idx,end_freq_idx)):
+    for freq_idx in reversed(range(start_freq_idx,end_freq_idx+1)):
         for idx, t in enumerate(np.arange(start_time,stop_time,dt)):
             y.append(freqs[freq_idx])
             time.append(t)
         start_time=stop_time
         stop_time=start_time+ioi   
-        
-    plt.plot(time,y)
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
-    plt.title('Chromatic Scale, A4: 440 Hz')
-    plt.show()
+    
+    if plot==True:    
+        plt.plot(time,y)
+        plt.xlabel('Time (s)')
+        plt.ylabel('Frequency (Hz)')
+        plt.title('Chromatic Scale, A4: 440 Hz')
+        plt.show()
 
     return time, y
 
 if __name__=='__main__':
-    iois=[0.250,0.125]
-    for ioi in iois:
-        #meas_start=68.8 Note: C#2/Db2, 69.30  Hz, Index: 25
-        #meas_end=524.3  Note: C5     , 523.25 Hz, Index: 60
-        plot_chromatic_scale(ioi,25,60)
+    #meas_start=68.8 Note: C#2/Db2, 69.30  Hz, Index: 25
+    #meas_end=524.3  Note: C5     , 523.25 Hz, Index: 60
+    chromatic_scale(0.250,25,60)

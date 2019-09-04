a5dd67fecc51ac2c8114e8de7414f606df7afca5
Craig Miller <craigdanielmiller@gmail.com>
2019-09-04 18:09:26 -0400

    Working POC for real-time pitch detection
    Successfully implemente 'queue' module to create separate python processes.

--- crepe_stream.py
+++ crepe_stream.py
@@ -2,31 +2,63 @@
 """
 Runs CREPE continuously on audio stream.
 """
-
 import sounddevice as sd
 import crepe
-#Use 'python -m sounddevice' to find device list (Webcam mic=1)
+import time as timelib
+import tensorflow as tf
+from multiprocessing import Process, Queue
+import sys
 
-t=10       #Recording Duration (Seconds)
-t_sample=1 #CREPE Period
-fs=16000   #Sample Rate (Hz)
+#Define sample parameters
+t_sample=0.5       #CREPE Sample Period
+fs=16000           #Sample Rate (Hz) - CREPE uses 16kHz
+crepe_step_size=50 #Step Size (ms)
+crepe_model='tiny' #Model Capacity ('tiny', 'small', 'medium', 'large', 'full')
+time_global=0      #Set start time (s)
 
-for sample in range(0,t+1,t_sample):
-    #Record sample
-    recording = sd.rec(int(t_sample*fs), samplerate=fs, channels=1)
-    sd.wait()
+#Initialize tensorflow GPU settings
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
+config = tf.ConfigProto(gpu_options=gpu_options)
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+def reader_proc(queue):
+    '''Read recording from queue, run CREPE, and print results.'''
+    ## Read from the queue; this will be spawned as a separate Process
+    #print('time, frequency, confidence')
+    global time_global
+    while True:
+        start=timelib.time()
+        recording = queue.get()
+        #Run CREPE on sample
+        time, frequency, confidence, _ = crepe.predict(audio=recording, sr=fs, 
+                                                   model_capacity=crepe_model,
+                                                   step_size=crepe_step_size)
+        #Adjust time
+        time+=time_global
+        i=0
+        while i<len(time):
+            print(time[i],frequency[i],confidence[i])
+            i+=1
+        time_global+=t_sample
+        end=timelib.time()
+        print('Reader & CREPE loop runtime: '+str(end-start)+' seconds.')
+
+def writer(queue):
+    '''Records audio sample (as nparray) and puts in queue.'''
+    while True:
+        #Record sample
+        recording = sd.rec(int(t_sample*fs), samplerate=fs, channels=1)
+        #Wait until finished recording
+        sd.wait()
+        #Write to the queue
+        queue.put(recording)
     
-    #Run CREPE on sample
-    time, frequency, confidence, _ = crepe.predict(audio=recording, 
-                                                   sr=fs, 
-                                                   model_capacity='full',
-                                                   step_size=20)
-    #Print Results
-    i=0
-    print('time, frequency, confidence')
-    while i<len(time):
-        print(time[i],frequency[i],confidence[i])
-        i+=1
-
-#print('Start Playing')
-#sd.playrec(recording, samplerate=fs, channels=1,blocking=True)
+if __name__=='__main__':
+    pqueue = Queue() # writer() writes to pqueue from _this_ process         
+    #Start reader as a separate process
+    reader_p = Process(target=reader_proc, args=((pqueue),))
+    reader_p.daemon = True
+    reader_p.start()        # Launch reader_proc() as a separate python process
+    writer(pqueue)          # Record Data
+    reader_p.join()         # Process Data

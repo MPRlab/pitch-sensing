6eba1727ac2d5c34fe1029cf354940587a170b2d
Craig Miller <craigdanielmiller@gmail.com>
2019-10-28 06:25:11 -0400

    Generated detection latency stats
    Generated detection latency stats

--- crepe_injection.py
+++ crepe_injection.py
@@ -9,30 +9,74 @@ import crepe
 import numpy as np
 import matplotlib.pyplot as plt
 from os import linesep
+from note_frequencies import freqs
+from math import log
 
 #Define sample parameters
-fs=16000           #Sample Rate (Hz) - CREPE uses 16kHz
+# fs=16000           #Sample Rate (Hz) - CREPE uses 16kHz
 crepe_step_size=1 #Step Size (ms)
 crepe_model='tiny' #Model Capacity ('tiny', 'small', 'medium', 'large', 'full')
-f1=200             #Hz
-f2=840           #Hz
-t_switch=0.4       #seconds
-t_total=10         #seconds
-#Onset results (300-400Hz)
-#Step Size, Latency
-#20ms: 40ms
-#10ms: 10ms
-#5ms:5ms (but conf=20)
-#2ms:4ms (but conf=22)
-#1ms:4ms (but conf=21.5)
+# f1=200             #Hz
+# f2=840           #Hz
+# t_switch=1       #seconds
+# t_total=2        #seconds
 
-#Onset results (400-300Hz)
-#Step Size, Latency
-#20ms:instant? (but conf=39)
-#10ms:instant? (but conf=39)
-#5ms:instant? (but conf=39)
-#2ms:instant? (but conf=39)
-#1ms:instant? (but conf=39)
+def detection_stats(time, frequency, confidence, f2, t_switch, conf_threshold=0.8):
+    '''Calculates CREPE detection time when shifting from f1 to f2.'''
+    #Find first time after switch that confidence is above conf_threshold
+    t_switch_idx=np.where(time == t_switch)[0][0]
+    idx=t_switch_idx
+    max_idx=len(time)-1
+    while confidence[idx]<conf_threshold:
+        idx+=1
+        if idx>max_idx:
+            break
+    if idx>max_idx:
+        latency=None
+        cents_error=None
+    else:
+        t_detect=time[idx]
+        #Calculate detection time
+        latency=t_detect-t_switch
+        #Calculate cents_error after confident detection
+        cents_error=1200*log(frequency[idx]/f2,2)
+
+    return latency, cents_error
+
+def all_detection_stats(freq_list,crepe_model,crepe_step_size,conf_threshold=0.65,t_switch=1,t_total=2,fs=16000,plot=False):
+    #For all ascending frequency shifts in range, print latency and error
+    max_idx=len(freq_list)-1
+    with open(f'{crepe_model}_'+f'{crepe_step_size}_'+r'all_detection_stats.txt', 'w') as file:
+        for idx, freq in freq_list.items():
+            if idx==max_idx:
+                break
+
+            #Ascend & Descend
+            methods=[[idx,idx+1],
+                     [idx+1,idx]]
+
+            for method in methods:
+                #Get frequencies
+                f1=freq_list[method[0]]
+                f2=freq_list[method[1]]
+
+                #Generate sin wave with two frequencies
+                t,rec_array=sin_wave_shift(f1,f2,t_switch,t_total,fs,plot)
+
+                #Run CREPE on sample
+                time, frequency, confidence, _ = crepe.predict(audio=rec_array, sr=fs, 
+                                                               model_capacity=crepe_model,
+                                                               step_size=crepe_step_size)
+                
+                #Save results
+                #save_results(time, frequency, confidence,filename='test.txt')
+
+                latency, cents_error=detection_stats(time, frequency, confidence, f2, t_switch, conf_threshold)
+
+                print(f1,f2,latency,cents_error)
+                    
+                file.write(f"f1: {f1}, f2: {f2}, latency: {latency}, cents_error: {cents_error}" + linesep)
+    file.close()
 
 def plot(t,y):
     plt.plot(t,y)
@@ -72,13 +116,12 @@ def save_results(time, frequency, confidence,filename='results.txt'):
     file.close()
 
 if __name__=='__main__':
-    #Generate sin wave with two frequencies
-    t,rec_array=sin_wave_shift(f1,f2,t_switch,t_total,plot=False)
 
-    #Run CREPE on sample
-    time, frequency, confidence, _ = crepe.predict(audio=rec_array, sr=fs, 
-                                                   model_capacity=crepe_model,
-	                                               step_size=crepe_step_size)
-    
-    #Save results
-    save_results(time, frequency, confidence)
+    models=['tiny', 'small', 'medium', 'large', 'full']
+    timesteps=[1,2,5,10]
+
+    for model in models:
+        for timestep in timesteps:
+            all_detection_stats(freqs,model,timestep,conf_threshold=0.65,t_switch=1,t_total=2,plot=False)
+
+    #Graph results
